2f90de543d5c93c9000767d1fe5c073cceea0892
Angie Chiang <angieyayabird@gmail.com>
2014-12-06 16:52:09 -0500

    modify function name

--- WebContent/search.js
+++ WebContent/search.js
@@ -4,7 +4,10 @@
 
 var myApp = angular.module('searchApp', []);
 myApp.controller('searchCtrl', function($scope) {
+    $scope.autoComplete = function(){
+          console.log("type " + $scope.query);
+    };
     $scope.search = function(){
           console.log("search " + $scope.query);
-    };
+    }
 });
