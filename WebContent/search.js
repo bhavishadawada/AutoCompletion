6dda9368fa1f7fb4813be5bd485f434b485c6bf2
Angie Chiang <angieyayabird@gmail.com>
2014-12-06 21:37:38 -0500

    front end search ok

--- WebContent/search.js
+++ WebContent/search.js
@@ -5,19 +5,23 @@
 var myApp = angular.module('searchApp', []);
 myApp.controller('searchCtrl', function($scope, $http) {
 	  var xmlhttp;
+	  $scope.content = "";
     $scope.autoComplete = function(){
           console.log("type " + $scope.query);
     };
     $scope.search = function(){
       console.log("search " + $scope.query);
       xmlhttp=new XMLHttpRequest();
-      xmlhttp.open("GET","http://localhost:25811/search?query="+$scope.query+"&ranker=favorite",true);
+      xmlhttp.open("GET","http://localhost:25811/search?query="+$scope.query+"&ranker=favorite&format=html",true);
       xmlhttp.onreadystatechange = handler;
       xmlhttp.send();
     };
     
-    var handler = function(){
-    	console.log("handler");
-    	console.log(xmlhttp.responseText);
+    function handler(){
+      if (xmlhttp.readyState==4 && xmlhttp.status==200)
+      {
+      	$scope.content = xmlhttp.responseText;
+     		$scope.$apply();
+      }
     }
 });
