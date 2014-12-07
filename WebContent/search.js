7c214038a7675cc610ea6064c23c0786ce552b17
Angie Chiang <angieyayabird@gmail.com>
2014-12-06 19:16:55 -0500

    frontend with request

--- WebContent/search.js
+++ WebContent/search.js
@@ -3,11 +3,21 @@
  */
 
 var myApp = angular.module('searchApp', []);
-myApp.controller('searchCtrl', function($scope) {
+myApp.controller('searchCtrl', function($scope, $http) {
+	  var xmlhttp;
     $scope.autoComplete = function(){
           console.log("type " + $scope.query);
     };
     $scope.search = function(){
-          console.log("search " + $scope.query);
+      console.log("search " + $scope.query);
+      xmlhttp=new XMLHttpRequest();
+      xmlhttp.open("GET","http://localhost:25811/search?query="+$scope.query+"&ranker=favorite",true);
+      xmlhttp.onreadystatechange = handler;
+      xmlhttp.send();
+    };
+    
+    var handler = function(){
+    	console.log("handler");
+    	console.log(xmlhttp.responseText);
     }
 });
